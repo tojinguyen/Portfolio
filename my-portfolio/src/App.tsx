d10a92c11298a45965364326a2fac4b4a4962955
tojinguyen <toainguyenprogramminggame@gmail.com>
2024-11-13 11:14:35 +0700

    install tailwindcss

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+
+/**
+ * Renders the main application component.
+ *
+ * @returns A JSX element containing a styled "Hello world!" heading.
+ */
+function App() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
+export default App
